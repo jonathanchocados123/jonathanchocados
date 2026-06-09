6d05f3c645165643237c40b47c767b7da229a62a
jonathanchocados123 <jonathanchocados@gmail.com>
2026-06-08 22:08:56 -0500

    Update car-data.ts

--- lib/car-data.ts
+++ lib/car-data.ts
@@ -16,10 +16,115 @@ export interface Car {
 export const cars: Car[] = [
     {
     id: 1,
+    marca: "Mitsubishi",
+    modelo: "L200",
+    año: 2019,
+    precio: "$9.990.000",
+    kilometraje: "63 mil kilómetros",
+    estado: "Se va andando",
+    transmision: "Manual",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 17 millones.",
+    nuevo: true,
+    imagenes: ["/110.jpg", "/111.jpg", "/112.jpg", "/113.jpg", "/88.jpg"]
+  },
+  {
+    id: 2,
+    marca: "Toyota",
+    modelo: "Rush",
+    año: 2022,
+    precio: "$6.490.000",
+    kilometraje: "90 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "Activo airbag",
+    descripcion: "Seguro pagó 13 millones.",
+    nuevo: true,
+    imagenes: ["/54.jpg", "/63.jpg", "/69.jpg", "/75.jpg"]
+  },
+   {
+    id: 3,
+    marca: "Mazda",
+    modelo: "CX-5 GT",
+    año: 2013,
+    precio: "$7.990.000",
+    kilometraje: "190 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "Recuperada de robo",
+    descripcion: "Seguro pagó 10 millones.",
+    nuevo: true,
+    imagenes: ["/11.jpg", "/74.jpg", "/4.jpg", "/10.jpg" ]
+  },
+ {
+    id: 4,
+    marca: "Suzuki",
+    modelo: "S Cross Limited",
+    año: 2016,
+    precio: "$5.490.000",
+    kilometraje: "190 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No Activo airbag",
+    descripcion: "Seguro pagó 9.5 millones.",
+    nuevo: true,
+    imagenes: ["/91.jpg", "/39.jpg", "/40.jpg", "/41.jpg", "/42.jpg"]
+  },
+   {
+    id: 5,
+    marca: "Renault",
+    modelo: "Oroch",
+    año: 2018,
+    precio: "$4.990.000",
+    kilometraje: "80 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "Activo airbag",
+    descripcion: "Seguro pagó 9 millones.",
+    nuevo: true,
+    imagenes: ["/93.jpg", "/94.jpg", "/95.jpg", "/30.jpg"]
+  },
+  {
+    id: 6,
+    marca: "Jmc",
+    modelo: "Vigus Plus 4x4",
+    año: 2023,
+    precio: "$6.990.000",
+    kilometraje: "55 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "Activo airbag",
+    descripcion: "Seguro pagó 11 millones.",
+    nuevo: true, 
+    imagenes: ["/38.jpg", "/49.jpg", "/44.jpg", "/24.jpg"]
+  },
+{
+    id: 7,
+    marca: "Honda",
+    modelo: "Pilot Ex",
+    año: 2011,
+    precio: "$4.990.000",
+    kilometraje: "140 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "Activo airbag",
+    descripcion: "Seguro pagó 8 millones.",
+    nuevo: true,
+    imagenes: ["/45.jpg", "/46.jpg", "/47.jpg", "/48.jpg"]
+  },
+  {
+    id: 8,
+    marca: "Furgón Jac",
+    modelo: "Refine",
+    año: 2023,
+    precio: "$8.990.000",
+    kilometraje: "160 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "Activo airbag",
+    descripcion: "Seguro pagó 15 millones.",
+    nuevo: true,
+    imagenes: ["/16.jpg", "/17.jpg", "/18.jpg", "/19.jpg"]
+  },
+  {
+    id: 9,
     marca: "Mazda",
     modelo: "Cx30 GtX",
     año: 2021,
-    precio: "$11.990.000",
+    precio: "$11.690.000",
     kilometraje: "105 mil kilómetros",
     estado: "Se va andando",
     airbag: "No activo airbag",
@@ -27,39 +132,12 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/64.jpg", "/65.jpg", "/66.jpg", "/67.jpg", "/68.jpg"]
   },
- {
-    id: 2,
-    marca: "BMW",
-    modelo: "X1 XDrive20D",
-    año: 2016,
-    precio: "$10.990.000",
-    kilometraje: "96 mil kilómetros",
-    estado: "Se va andando",
-    transmision: "Automatica",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 17 millones.",
-    nuevo: true,
-    imagenes: ["/110.jpg", "/111.jpg", "/112.jpg", "/113.jpg", "/88.jpg"]
-  },
   {
-    id: 3,
-    marca: "Mazda",
-    modelo: "Cx5 R",
-    año: 2022,
-    precio: "$9.490.000",
-    kilometraje: "70 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No Activo airbag",
-    descripcion: "Seguro pagó 18 millones.",
-    nuevo: true,
-    imagenes: ["/54.jpg", "/63.jpg", "/69.jpg", "/75.jpg", "/77.jpg"]
-  },
-  {
-    id: 4,
+    id: 10,
     marca: "Volkswagen",
     modelo: "Tiguan",
     año: 2022,
-    precio: "$10.990.000",
+    precio: "$10.690.000",
     kilometraje: "60 mil kilómetros",
     estado: "Se va andando",
     airbag: "No Activo airbag",
@@ -68,11 +146,11 @@ export const cars: Car[] = [
     imagenes: ["/58.jpg", "/102.jpg", "/79.jpg", "/80.jpg", "/89.jpg"]
   },
    {
-    id: 5,
+    id: 11,
     marca: "Peugeot",
     modelo: "3008",
     año: 2019,
-    precio: "$7.990.000",
+    precio: "$6.990.000",
     kilometraje: "120 mil kilómetros",
     estado: "Se va andando",
     airbag: "Activo airbag",
@@ -81,11 +159,11 @@ export const cars: Car[] = [
     imagenes: ["/50.jpg", "/51.jpg", "/52.jpg", "/53.jpg"]
   },
    {
-    id: 6,
+    id: 12,
     marca: "Suzuki",
     modelo: "Scross Glx",
     año: 2019,
-    precio: "$7.990.000",
+    precio: "$7.690.000",
     kilometraje: "70 mil kilómetros",
     estado: "Se va andando",
     airbag: "Activo airbag de cortina y asiento",
@@ -94,11 +172,11 @@ export const cars: Car[] = [
     imagenes: ["/31.jpg", "/34.jpg", "/37.jpg", "/43.jpg", "/57.jpg"]
   },
     {
-    id: 7,
+    id: 13,
     marca: "Great Wall",
     modelo: "M4",
     año: 2019,
-    precio: "$3.590.000",
+    precio: "$2.990.000",
     kilometraje: "50 mil kilómetros",
     estado: "Se va andando",
     airbag: "Activo airbag",
@@ -107,11 +185,11 @@ export const cars: Car[] = [
     imagenes: ["/55.jpg", "/56.jpg", "/84.jpg"]
   },
   {
-    id: 8,
+    id: 14,
     marca: "Chevrolet",
     modelo: "Groove Premier",
     año: 2023,
-    precio: "$6.990.000",
+    precio: "$5.990.000",
     kilometraje: "9 mil kilómetros",
     estado: "Se va andando",
     airbag: "Activo airbag",
@@ -120,11 +198,11 @@ export const cars: Car[] = [
     imagenes: ["/90.jpg", "/86.jpg", "/103.jpg"]
   },
     {
-    id: 9,
+    id: 15,
     marca: "Nissan",
     modelo: "Note",
     año: 2014,
-    precio: "$3.990.000",
+    precio: "$2.990.000",
     kilometraje: "90 mil kilómetros",
     estado: "Se va andando",
     airbag: "Activo airbag",
@@ -132,25 +210,12 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/104.jpg", "/105.jpg", "/106.jpg"]
   },
-     {
-    id: 10,
-    marca: "Volkswagen",
-    modelo: "Tiguan",
-    año: 2014,
-    precio: "$5.590.000",
-    kilometraje: "270 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No Activo airbag",
-    descripcion: "Seguro pagó 9 millones.",
-    nuevo: true,
-    imagenes: ["/11.jpg", "/74.jpg", "/4.jpg", "/10.jpg" ]
-  },
-     {
-    id: 11,
+  {
+    id: 16,
     marca: "Changan",
     modelo: "Alsvin",
     año: 2021,
-    precio: "$5.590.000",
+    precio: "$4.990.000",
     kilometraje: "37 mil kilómetros",
     estado: "Se va andando",
     airbag: "No Activo airbag",
@@ -158,24 +223,12 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/83.jpg", "/73.jpg", "/70.jpg", "/71.jpg"]
   },
-     {
-    id: 12,
-    marca: "Cf Moto",
-    modelo: "Cf800 Mt",
-    año: 2023,
-    precio: "$4.990.000",
-    kilometraje: "32 mil kilómetros",
-    estado: "Se va andando",
-    descripcion: "Seguro pagó 9 millones.",
-    nuevo: true,
-    imagenes: ["/91.jpg", "/39.jpg", "/40.jpg", "/41.jpg", "/42.jpg", "/76.jpg"]
-  },
-     {
-    id: 13,
+  {
+    id: 17,
     marca: "Renault",
     modelo: "Symbol",
     año: 2017,
-    precio: "$4.690.000",
+    precio: "$3.990.000",
     kilometraje: "50 mil kilómetros",
     estado: "Se va andando",
     airbag: "No Activo airbag",
@@ -183,38 +236,12 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/12.jpg", "/13.jpg", "/14.jpg", "/15.jpg", "/72.jpg"]
   },
- {
-    id: 14,
-    marca: "Suzuki",
-    modelo: "Baleno Glx",
-    año: 2018,
-    precio: "$6.490.000",
-    kilometraje: "80 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No Activo airbag",
-    descripcion: "Seguro pagó 10 millones.",
-    nuevo: true,
-    imagenes: ["/93.jpg", "/94.jpg", "/95.jpg", "/30.jpg"]
-  },
   {
-    id: 15,
-    marca: "Chevrolet",
-    modelo: "Groove Ltz",
-    año: 2024,
-    precio: "$6.490.000",
-    kilometraje: "41 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No Activo airbag",
-    descripcion: "Seguro pagó 12 millones.",
-    nuevo: true, 
-    imagenes: ["/38.jpg", "/49.jpg", "/44.jpg", "/24.jpg", "/92.jpg"]
-  },
-   {
-    id: 16,
+    id: 18,
     marca: "Volkswagen",
     modelo: "Amarok Highline",
     año: 2019,
-    precio: "$14.490.000",
+    precio: "$13.490.000",
     kilometraje: "150 mil kilómetros",
     estado: "Se va andando",
     airbag: "Activo airbag de cortina y asiento",
@@ -223,44 +250,17 @@ export const cars: Car[] = [
     imagenes: ["/78.jpg", "/81.jpg",  "/82.jpg", "/85.jpg", "/87.jpg"]
   },
   {
-    id: 17,
+    id: 19,
     marca: "Mercedes Benz",
     modelo: "ML 350",
     año: 2012,
-    precio: "$6.990.000",
+    precio: "$6.590.000",
     kilometraje: "220 mil kilómetros",
     estado: "Se va andando",
     airbag: "Activo airbag",
     descripcion: "Seguro pagó 13 millones.",
     nuevo: true,
     imagenes: ["/59.jpg", "/60.jpg", "/61.jpg", "/62.jpg"]
-  },
-
-{
-    id: 18,
-    marca: "Ford",
-    modelo: "Territory Titanium",
-    año: 2023,
-    precio: "$6.990.000",
-    kilometraje: "70 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "Activo airbag",
-    descripcion: "Seguro pagó 13 millones.",
-    nuevo: true,
-    imagenes: ["/45.jpg", "/46.jpg", "/47.jpg", "/48.jpg"]
-  },
-  {
-    id: 19,
-    marca: "Subaru",
-    modelo: "Impreza",
-    año: 2021,
-    precio: "$7.490.000",
-    kilometraje: "100 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "Activo airbag",
-    descripcion: "Seguro pagó 15 millones.",
-    nuevo: true,
-    imagenes: ["/16.jpg", "/17.jpg", "/18.jpg", "/19.jpg", "/28.jpg"]
   },
   {
     id: 20,
@@ -291,19 +291,6 @@ export const cars: Car[] = [
   },
   {
     id: 22,
-    marca: "Nissan",
-    modelo: "Murano",
-    año: 2013,
-    precio: "$4.290.000",
-    kilometraje: "160 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "Activo airbag de cortina y asiento",
-    descripcion: "Seguro pagó 9 millones.",
-    nuevo: true,
-    imagenes: ["/96.jpg", "/97.jpg", "/98.jpg", "/99.jpg"]
-  },
-  {
-    id: 23,
     marca: "Volvo",
     modelo: "S60 T3",
     año: 2019,
@@ -314,6 +301,19 @@ export const cars: Car[] = [
     descripcion: "Seguro pagó 12 millones.",
     nuevo: true,
     imagenes: ["/29.jpg", "/35.jpg", "/36.jpg", "/126.jpg"]
+  },
+     {
+    id: 23,
+    marca: "Nissan",
+    modelo: "Murano",
+    año: 2013,
+    precio: "$4.290.000",
+    kilometraje: "160 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "Activo airbag de cortina y asiento",
+    descripcion: "Seguro pagó 9 millones.",
+    nuevo: true,
+    imagenes: ["/96.jpg", "/97.jpg", "/98.jpg", "/99.jpg"]
   },
   {
     id: 24,
@@ -368,7 +368,5 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/5.jpg", "/6.jpg", "/7.jpg", "/8.jpg","/9.jpg"]
   },
-
- 
-   
+  
 ]
