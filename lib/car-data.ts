62fb5cfd563c0ba5a1344b2d99b706e9e2f3e47c
jonathanchocados123 <jonathanchocados@gmail.com>
2026-07-22 20:02:46 -0500

    Update car-data.ts

--- lib/car-data.ts
+++ lib/car-data.ts
@@ -16,6 +16,84 @@ export interface Car {
 export const cars: Car[] = [
    {
     id: 1,
+    marca: "BMW",
+    modelo: "116i",
+    año: 2013,
+    precio: "$5.890.000",
+    kilometraje: "170 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "Np activo airbag",
+    descripcion: "Seguro pagó 10 millones.",
+    nuevo: true,
+    imagenes: ["/104.jpg", "/105.jpg", "/106.jpg", "/28.jpg"]
+  },
+ {
+    id: 2,
+    marca: "Volkswagen",
+    modelo: "Golf HighLine",
+    año: 2018,
+    precio: "$6.990.000",
+    kilometraje: "120 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "Recuperado de robo, Sin llave, comprador hace la llave",
+    descripcion: "Seguro pagó 14 millones.",
+    nuevo: true,
+    imagenes: ["/93.jpg", "/29.jpg", "/31.jpg", "/32.jpg", "/53.jpg"]
+  },
+   {
+    id: 3,
+    marca: "Honda",
+    modelo: "City LX",
+    año: 2019,
+    precio: "$4.690.000",
+    kilometraje: "113 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 9.5 millones.",
+    nuevo: true,
+    imagenes: ["/78.jpg", "/81.jpg",  "/82.jpg", "/85.jpg"]
+  },
+          {
+    id: 4,
+    marca: "Volkswagen",
+    modelo: "Polo",
+    año: 2023,
+    precio: "$6.490.000",
+    kilometraje: "30 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 12 millones.",
+    nuevo: true,
+    imagenes: ["/83.jpg", "/73.jpg", "/70.jpg", "/71.jpg", "/21.jpg"]
+  },
+    {
+    id: 5,
+    marca: "Chevrolet",
+    modelo: "Ónix Ltz",
+    año: 2019,
+    precio: "$4.990.000",
+    kilometraje: "120 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 8.5 millones.",
+    nuevo: true,
+    imagenes: ["/36.jpg", "/38.jpg", "/39.jpg", "/40.jpg", "/35.jpg"]
+  },
+   {
+    id: 6,
+    marca: "Renault",
+    modelo: "Fluence",
+    año: 2017,
+    precio: "$2.990.000",
+    kilometraje: "70 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 6.5 millones.",
+    nuevo: true,
+    imagenes: ["/45.jpg", "/46.jpg", "/47.jpg"]
+  },
+   {
+    id: 7,
     marca: "Mazda",
     modelo: "CX-5",
     año: 2026,
@@ -28,7 +106,7 @@ export const cars: Car[] = [
     imagenes: ["/58.jpg", "/102.jpg", "/79.jpg", "/49.jpg"]
   },
  {
-    id: 2,
+    id: 8,
     marca: "Peugeot",
     modelo: "Parnert",
     año: 2014,
@@ -41,59 +119,7 @@ export const cars: Car[] = [
     imagenes: ["/67.jpg", "/68.jpg", "/121.jpg", "/122.jpg"]
   },
   {
-    id: 3,
-    marca: "Peugeot",
-    modelo: "208",
-    año: 2025,
-    precio: "$7.990.000",
-    kilometraje: "7 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "Np activo airbag",
-    descripcion: "Seguro pagó 14 millones.",
-    nuevo: true,
-    imagenes: ["/104.jpg", "/105.jpg", "/106.jpg", "/28.jpg", "/35.jpg"]
-  },
-     {
-    id: 4,
-    marca: "Dodge",
-    modelo: "Journey Sxt",
-    año: 2011,
-    precio: "$3.990.000",
-    kilometraje: "170 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 8 millones.",
-    nuevo: true,
-    imagenes: ["/93.jpg", "/29.jpg", "/31.jpg", "/32.jpg", "/53.jpg"]
-  },
-          {
-    id: 5,
-    marca: "Suzuki",
-    modelo: "Grand Vitara Sport Glx",
-    año: 2015,
-    precio: "$4.790.000",
-    kilometraje: "150 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 9 millones.",
-    nuevo: true,
-    imagenes: ["/83.jpg", "/73.jpg", "/70.jpg", "/71.jpg"]
-  },
-  {
-    id: 6,
-    marca: "Volkswagen",
-    modelo: "Virtus",
-    año: 2023,
-    precio: "$6.890.000",
-    kilometraje: "57 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "Activo airbag de asiento",
-    descripcion: "Seguro pagó 10 millones.",
-    nuevo: true,
-    imagenes: ["/36.jpg", "/38.jpg", "/39.jpg", "/40.jpg"]
-  },
-      {
-    id: 7,
+    id: 9,
     marca: "Chevrolet",
     modelo: "Sail",
     año: 2019,
@@ -105,8 +131,8 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/89.jpg", "/90.jpg", "/86.jpg", "/103.jpg"]
   },
-      {
-    id: 8,
+  {
+    id: 10,
     marca: "Changan",
     modelo: "Cs15 Luxury",
     año: 2022,
@@ -118,8 +144,8 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/94.jpg", "/95.jpg", "/4.jpg", "/30.jpg"]
   },
-        {
-    id: 9,
+  {
+    id: 11,
     marca: "Hyundai",
     modelo: "Accent",
     año: 2021,
@@ -132,20 +158,7 @@ export const cars: Car[] = [
     imagenes: ["/12.jpg", "/13.jpg", "/62.jpg", "/10.jpg", "/20.jpg"]
   },
   {
-    id: 10,
-    marca: "Mazda",
-    modelo: "6 GT",
-    año: 2016,
-    precio: "$4.790.000",
-    kilometraje: "160 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "Activo airbag de cortina y asiento",
-    descripcion: "Seguro pagó 11 millones.",
-    nuevo: true,
-    imagenes: ["/78.jpg", "/81.jpg",  "/82.jpg", "/85.jpg"]
-  },
-    {
-    id: 11,
+    id: 12,
     marca: "Nissan",
     modelo: "Qashqai",
     año: 2016,
@@ -157,20 +170,6 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/118.jpg", "/119.jpg", "/120.jpg", "/23.jpg", "/24.jpg"]
   },
-  {
-    id: 12,
-    marca: "Ford",
-    modelo: "Territory Titanium",
-    año: 2022,
-    precio: "$7.790.000",
-    kilometraje: "100 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 15 millones.",
-    nuevo: true,
-    imagenes: ["/45.jpg", "/46.jpg", "/47.jpg", "/48.jpg", "/80.jpg"]
-  },
-
  {
     id: 13,
     marca: "Toyota",
@@ -212,19 +211,6 @@ export const cars: Car[] = [
   },
    {
     id: 16,
-    marca: "Suzuki",
-    modelo: "Grand Nomade Glx",
-    año: 2015,
-    precio: "$5.790.000",
-    kilometraje: "137 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 10 millones.",
-    nuevo: true,
-    imagenes: ["/50.jpg", "/51.jpg", "/52.jpg"]
-  },
-  {
-    id: 17,
     marca: "Toyota",
     modelo: "Yaris Spor",
     año: 2019,
@@ -237,7 +223,7 @@ export const cars: Car[] = [
     imagenes: ["/11.jpg", "/64.jpg", "/65.jpg", "/66.jpg"]
   },
    {
-    id: 18,
+    id: 17,
     marca: "Kia",
     modelo: "Seltos",
     año: 2023,
@@ -250,7 +236,7 @@ export const cars: Car[] = [
     imagenes: ["/1.jpg", "/2.jpg", "/3.jpg"]
   },
   {
-    id: 19,
+    id: 18,
     marca: "Chevrolet",
     modelo: "Captiva",
     año: 2018,
@@ -264,7 +250,7 @@ export const cars: Car[] = [
   },
 
  {
-    id: 20,
+    id: 19,
     marca: "Chery",
     modelo: "Tiggo 2",
     año: 2022,
@@ -278,7 +264,7 @@ export const cars: Car[] = [
     imagenes: ["/cherytigo2-4.jpg","/cherytigo2-3.jpg","/cherytigo2-2.jpg","/cherytigo2-1.jpg"]
   },
    {
-    id: 21,
+    id: 20,
     marca: "Buggy ",
     modelo: "Can-Am  maverick X3 Turbo",
     año: 2020,
@@ -290,7 +276,7 @@ export const cars: Car[] = [
     imagenes: ["/Buggy5.jpg", "/Buggy4.jpg", "/Buggy3.jpg","/Buggy2.jpg", "/Buggy1.jpg", "/Buggy.jpg"]
   },
  {
-    id: 22,
+    id: 21,
     marca: "Dodge",
     modelo: "Dakota Sxt ",
     año: 2011,
