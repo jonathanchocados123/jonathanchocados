9ae58f75bf4935c7d1c78373a8f5d59372b7f70d
jonathanchocados123 <jonathanchocados@gmail.com>
2026-09-03 11:18:31 -0500

    Update car-data.ts

--- lib/car-data.ts
+++ lib/car-data.ts
@@ -16,20 +16,138 @@ export interface Car {
 export const cars: Car[] = [
   {
     id: 1,
-    marca: "Volkswagen",
-    modelo: "Golf 1.6",
-    año: 2017,
-    precio: "$3.990.000",
-    kilometraje: "125 mil kilómetros",
+    marca: "BMW",
+    modelo: "XL",
+    año: 2014,
+    precio: "$8.490.000",
+    kilometraje: "140 mil kilómetros",
     estado: "Se va andando",
-    airbag: "Activo airbag",
+    airbag: "No activo airbag",
     transmision: "Automatico",
-    descripcion: "Seguro pagó 9 millones.",
+    descripcion: "Seguro pagó 14 millones.",
     nuevo: true,
     imagenes: ["/34.jpg", "/117.jpg", "/22.jpg", "/35.jpg", "/12.jpg"]
   },
-  {
+ {
     id: 2,
+    marca: "Nissan",
+    modelo: "Xtrail",
+    año: 2023,
+    precio: "$15.900.000",
+    kilometraje: "19 mil kilómetros",
+    estado: "Se va andando",
+    transmision: "Automatico",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 31 millones.",
+    nuevo: true,
+    imagenes: ["/41.jpg", "/42.jpg", "/43.jpg", "/44.jpg", "/16.jpg"]
+  },
+    {
+    id: 3,
+    marca: "Peugeot",
+    modelo: "208",
+    año: 2018,
+    precio: "$5.990.000",
+    kilometraje: "108 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 10 millones.",
+    nuevo: true,
+    imagenes: ["/14.jpg", "/15.jpg", "/59.jpg", "/83.jpg", "/73.jpg"]
+  },
+   {
+    id: 4,
+    marca: "Volvo",
+    modelo: "S60 T6",
+    año: 2013,
+    precio: "$6.490.000",
+    kilometraje: "170 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "Activo airbag de asiento",
+    descripcion: "Seguro pagó 9 millones.",
+    nuevo: true,
+    imagenes: ["/55.jpg", "/110.jpg", "/113.jpg", "/115.jpg", "/61.jpg"]
+  },
+ {
+    id: 5,
+    marca: "Kia",
+    modelo: "Soluto",
+    año: 2024,
+    precio: "$4.990.000",
+    kilometraje: "1700 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "Activo airbag",
+    descripcion: "Seguro pagó 10 millones.",
+    nuevo: true,
+    imagenes: ["/70.jpg", "/71.jpg", "/93.jpg", "/29.jpg"]
+  },
+ {
+    id: 6,
+    marca: "Changan",
+    modelo: "Alsvin",
+    año: 2021,
+    precio: "$4.490.000",
+    kilometraje: "44 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 8 millones.",
+    nuevo: true,
+    imagenes: ["/21.jpg",  "/1.jpg", "/108.jpg", "/109.jpg"]
+  },
+ {
+    id: 7,
+    marca: "Chery",
+    modelo: "Tiggo 7 pro",
+    año: 2022,
+    precio: "$7.490.000",
+    kilometraje: "115 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 14 millones.",
+    nuevo: true,
+    imagenes: ["/111.jpg", "/112.jpg", "/36.jpg", "/38.jpg"]
+  },
+   {
+    id: 8,
+    marca: "Suzuki",
+    modelo: "Baleno",
+    año: 2020,
+    precio: "$5.590.000",
+    kilometraje: "34 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 9 millones.",
+    nuevo: true,
+    imagenes: ["/39.jpg", "/40.jpg", "/80.jpg", "/81.jpg", "/82.jpg"]
+  },
+   {
+    id: 9,
+    marca: "Ssangyong",
+    modelo: "Actyon Sport",
+    año: 2018,
+    precio: "$6.990.000",
+    kilometraje: "198 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 10 millones.",
+    nuevo: true,
+    imagenes: ["/85.jpg", "/114.jpg", "/76.jpg", "/77.jpg"]
+  },
+   {
+    id: 10,
+    marca: "Hyundai",
+    modelo: "Accent",
+    año: 2023,
+    precio: "$6.490.000",
+    kilometraje: "140 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 11 millones.",
+    nuevo: true,
+    imagenes: ["/31.jpg", "/32.jpg", "/19.jpg", "/84.jpg"]
+  },
+  {
+    id: 11,
     marca: "Hyundai",
     modelo: "Grand i10",
     año: 2025,
@@ -42,7 +160,7 @@ export const cars: Car[] = [
     imagenes: ["/37.jpg", "/116.jpg", "/123.jpg", "/87.jpg"]
   },
 {
-    id: 3,
+    id: 12,
     marca: "Kia",
     modelo: "Soluto",
     año: 2020,
@@ -54,22 +172,8 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/92.jpg", "/100.jpg", "/101.jpg", "/107.jpg", "/60.jpg"]
   },
- {
-    id: 4,
-    marca: "Citroen",
-    modelo: "C3",
-    año: 2024,
-    precio: "$4.900.000",
-    kilometraje: "29 mil kilómetros",
-    estado: "Se va andando",
-    transmision: "Manual",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 12 millones.",
-    nuevo: true,
-    imagenes: ["/41.jpg", "/42.jpg", "/43.jpg", "/44.jpg", "/16.jpg"]
-  },
   {
-    id: 5,
+    id: 13,
     marca: "Nissan",
     modelo: "Xtrail",
     año: 2020,
@@ -82,7 +186,7 @@ export const cars: Car[] = [
     imagenes: ["/103.jpg", "/25.jpg", "/26.jpg", "/20.jpg", "/53.jpg"]
   },
    {
-    id: 6,
+    id: 14,
     marca: "Chevrolet",
     modelo: "Ónix Rs",
     año: 2023,
@@ -95,7 +199,7 @@ export const cars: Car[] = [
     imagenes: ["/86.jpg", "/45.jpg", "/46.jpg", "/47.jpg"]
   },
    {
-    id: 7,
+    id: 15,
     marca: "Mazda",
     modelo: "Cx5 R",
     año: 2019,
@@ -108,20 +212,7 @@ export const cars: Car[] = [
     imagenes: ["/2.jpg", "/3.jpg", "/124.jpg"]
   },
   {
-    id: 8,
-    marca: "Chevrolet",
-    modelo: "Prisma Ltz",
-    año: 2018,
-    precio: "$2.590.000",
-    kilometraje: "48 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "Activo airbag",
-    descripcion: "Seguro pagó 8 millones.",
-    nuevo: true,
-    imagenes: ["/14.jpg", "/15.jpg", "/59.jpg"]
-  },
-  {
-    id: 9,
+    id: 16,
     marca: "Seat",
     modelo: "Ateca",
     año: 2024,
@@ -134,7 +225,7 @@ export const cars: Car[] = [
     imagenes: ["/4.jpg", "/11.jpg", "/27.jpg", "/28.jpg"]
   },
    {
-    id: 10,
+    id: 17,
     marca: "Nissan",
     modelo: "Sentra",
     año: 2012,
@@ -147,20 +238,7 @@ export const cars: Car[] = [
     imagenes: ["/64.jpg", "/65.jpg", "/66.jpg", "/67.jpg"]
   },
    {
-    id: 11,
-    marca: "BMW",
-    modelo: "320D Luxury",
-    año: 2014,
-    precio: "$6.790.000",
-    kilometraje: "280 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 16 millones.",
-    nuevo: true,
-    imagenes: ["/55.jpg", "/110.jpg", "/113.jpg", "/115.jpg", "/61.jpg"]
-  },
-  {
-    id: 12,
+    id: 18,
     marca: "Volvo",
     modelo: "Xc60 ll T5",
     año: 2019,
@@ -173,7 +251,7 @@ export const cars: Car[] = [
     imagenes: ["/24.jpg", "/50.jpg", "/51.jpg", "/52.jpg"]
   },
   {
-    id: 13,
+    id: 19,
     marca: "Mg",
     modelo: "Zs",
     año: 2025,
@@ -186,7 +264,7 @@ export const cars: Car[] = [
     imagenes: ["/18.jpg", "/13.jpg", "/62.jpg", "/10.jpg"]
   },
   {
-    id: 14,
+    id: 20,
     marca: "Volkswagen",
     modelo: "Nivus Tsi",
     año: 2023,
@@ -200,7 +278,7 @@ export const cars: Car[] = [
     imagenes: ["/54.jpg", "/63.jpg", "/69.jpg", "/75.jpg", "/17.jpg"]
   },
   {
-    id: 15,
+    id: 21,
     marca: "Chery",
     modelo: "Tiggo 2",
     año: 2022,
@@ -214,7 +292,7 @@ export const cars: Car[] = [
     imagenes: ["/cherytigo2-4.jpg","/cherytigo2-3.jpg","/cherytigo2-2.jpg","/cherytigo2-1.jpg"]
   },
    {
-    id: 16,
+    id: 22,
     marca: "Buggy ",
     modelo: "Can-Am  maverick X3 Turbo",
     año: 2020,
