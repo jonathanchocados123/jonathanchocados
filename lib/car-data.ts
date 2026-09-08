7bc0245573384078e98ec2bcd5f6ad41855ca451
jonathanchocados123 <jonathanchocados@gmail.com>
2026-09-07 19:11:14 -0500

    Update car-data.ts

--- lib/car-data.ts
+++ lib/car-data.ts
@@ -19,7 +19,7 @@ export const cars: Car[] = [
     marca: "Suzuki",
     modelo: "Swift",
     año: 2025,
-    precio: "$7.990.000",
+    precio: "$6.990.000",
     kilometraje: "18 mil kilómetros",
     estado: "Se va andando",
     airbag: "No activo airbag",
@@ -29,23 +29,10 @@ export const cars: Car[] = [
   },
  {
     id: 2,
-    marca: "Volkswagen",
-    modelo: "Tiguan",
-    año: 2012,
-    precio: "$3.990.000",
-    kilometraje: "418 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 8 millones.",
-    nuevo: true,
-    imagenes: ["/70.jpg", "/71.jpg", "/93.jpg", "/29.jpg", "/91.jpg"]
-  },
- {
-    id: 3,
     marca: "Renault",
     modelo: "Koleos",
     año: 2015,
-    precio: "$3.990.000",
+    precio: "$3.490.000",
     kilometraje: "130 mil kilómetros",
     estado: "Se va andando",
     airbag: "Activo airbag",
@@ -54,11 +41,11 @@ export const cars: Car[] = [
     imagenes: ["/111.jpg", "/112.jpg", "/36.jpg", "/38.jpg", "/88.jpg"]
   },
      {
-    id: 4,
+    id: 3,
     marca: "Chery",
     modelo: "Tiggo 2 Pro",
     año: 2023,
-    precio: "$4.490.000",
+    precio: "$4.590.000",
     kilometraje: "38 mil kilómetros",
     estado: "Se va andando",
     airbag: "No activo airbag",
@@ -66,65 +53,12 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/39.jpg", "/40.jpg", "/80.jpg", "/81.jpg"]
   },
-   {
-    id: 5,
-    marca: "Suzuki",
-    modelo: "Grand Nomade Glx",
-    año: 2019,
-    precio: "$5.990.000",
-    kilometraje: "40 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 9 millones.",
-    nuevo: true,
-    imagenes: ["/86.jpg", "/45.jpg", "/46.jpg", "/47.jpg", "/82.jpg"]
-  },
-   {
-    id: 6,
-    marca: "Suzuki",
-    modelo: "Dzire",
-    año: 2019,
-    precio: "$3.990.000",
-    kilometraje: "130 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 10 millones.",
-    nuevo: true,
-    imagenes: ["/14.jpg", "/15.jpg", "/59.jpg", "/83.jpg", "/73.jpg"]
-  },
-   {
-    id: 7,
-    marca: "Nissan",
-    modelo: "Qashqai",
-    año: 2014,
-    precio: "$4.490.000",
-    kilometraje: "170 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 8 millones.",
-    nuevo: true,
-    imagenes: ["/85.jpg", "/114.jpg", "/76.jpg", "/77.jpg"]
-  },
   {
-    id: 8,
-    marca: "BMW",
-    modelo: "X1",
-    año: 2014,
-    precio: "$7.990.000",
-    kilometraje: "140 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    transmision: "Automatico",
-    descripcion: "Seguro pagó 14 millones.",
-    nuevo: true,
-    imagenes: ["/34.jpg", "/117.jpg", "/22.jpg", "/35.jpg", "/12.jpg"]
-  },
- {
-    id: 9,
+    id: 4,
     marca: "Nissan",
     modelo: "Xtrail",
     año: 2023,
-    precio: "$14.900.000",
+    precio: "$14.590.000",
     kilometraje: "19 mil kilómetros",
     estado: "Se va andando",
     transmision: "Automatico",
@@ -134,11 +68,11 @@ export const cars: Car[] = [
     imagenes: ["/41.jpg", "/42.jpg", "/43.jpg", "/44.jpg", "/16.jpg"]
   },
  {
-    id: 10,
+    id: 5,
     marca: "Changan",
     modelo: "Alsvin",
     año: 2021,
-    precio: "$3.990.000",
+    precio: "$3.690.000",
     kilometraje: "44 mil kilómetros",
     estado: "Se va andando",
     airbag: "No activo airbag",
@@ -147,7 +81,7 @@ export const cars: Car[] = [
     imagenes: ["/21.jpg",  "/1.jpg", "/108.jpg", "/109.jpg"]
   },
    {
-    id: 11,
+    id: 6,
     marca: "Hyundai",
     modelo: "Accent",
     año: 2023,
@@ -160,7 +94,7 @@ export const cars: Car[] = [
     imagenes: ["/31.jpg", "/32.jpg", "/19.jpg", "/84.jpg"]
   },
   {
-    id: 12,
+    id: 7,
     marca: "Hyundai",
     modelo: "Grand i10",
     año: 2025,
@@ -173,11 +107,11 @@ export const cars: Car[] = [
     imagenes: ["/37.jpg", "/116.jpg", "/123.jpg", "/87.jpg"]
   },
 {
-    id: 13,
+    id: 8,
     marca: "Kia",
     modelo: "Soluto",
     año: 2020,
-    precio: "$4.590.000",
+    precio: "$4.390.000",
     kilometraje: "128 mil kilómetros",
     estado: "Se va andando",
     airbag: "No activo airbag",
@@ -186,7 +120,7 @@ export const cars: Car[] = [
     imagenes: ["/92.jpg", "/100.jpg", "/101.jpg", "/107.jpg", "/60.jpg"]
   },
   {
-    id: 14,
+    id: 9,
     marca: "Nissan",
     modelo: "Xtrail",
     año: 2020,
@@ -199,7 +133,7 @@ export const cars: Car[] = [
     imagenes: ["/103.jpg", "/25.jpg", "/26.jpg", "/20.jpg", "/53.jpg"]
   },
    {
-    id: 15,
+    id: 10,
     marca: "Mazda",
     modelo: "Cx5 R",
     año: 2019,
@@ -212,7 +146,7 @@ export const cars: Car[] = [
     imagenes: ["/2.jpg", "/3.jpg", "/124.jpg"]
   },
   {
-    id: 16,
+    id: 11,
     marca: "Seat",
     modelo: "Ateca",
     año: 2024,
@@ -225,7 +159,7 @@ export const cars: Car[] = [
     imagenes: ["/4.jpg", "/11.jpg", "/27.jpg", "/28.jpg"]
   },
    {
-    id: 17,
+    id: 12,
     marca: "Nissan",
     modelo: "Sentra",
     año: 2012,
@@ -238,7 +172,7 @@ export const cars: Car[] = [
     imagenes: ["/64.jpg", "/65.jpg", "/66.jpg", "/67.jpg"]
   },
    {
-    id: 18,
+    id: 13,
     marca: "Volvo",
     modelo: "Xc60 ll T5",
     año: 2019,
@@ -251,20 +185,7 @@ export const cars: Car[] = [
     imagenes: ["/24.jpg", "/50.jpg", "/51.jpg", "/52.jpg"]
   },
   {
-    id: 19,
-    marca: "Mg",
-    modelo: "Zs",
-    año: 2025,
-    precio: "$4.790.000",
-    kilometraje: "10 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 11 millones.",
-    nuevo: true,
-    imagenes: ["/18.jpg", "/13.jpg", "/62.jpg", "/10.jpg"]
-  },
-  {
-    id: 20,
+    id: 14,
     marca: "Volkswagen",
     modelo: "Nivus Tsi",
     año: 2023,
@@ -278,7 +199,7 @@ export const cars: Car[] = [
     imagenes: ["/54.jpg", "/63.jpg", "/69.jpg", "/75.jpg", "/17.jpg"]
   },
   {
-    id: 21,
+    id: 15,
     marca: "Chery",
     modelo: "Tiggo 2",
     año: 2022,
@@ -291,18 +212,7 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/cherytigo2-4.jpg","/cherytigo2-3.jpg","/cherytigo2-2.jpg","/cherytigo2-1.jpg"]
   },
-   {
-    id: 22,
-    marca: "Buggy ",
-    modelo: "Can-Am  maverick X3 Turbo",
-    año: 2020,
-    precio: "$19.490.000",
-    kilometraje: "59 mil km",
-     estado: "Se va andando",
-    descripcion: "Extensiones de puertas.",
-    nuevo: true,
-    imagenes: ["/Buggy5.jpg", "/Buggy4.jpg", "/Buggy3.jpg","/Buggy2.jpg", "/Buggy1.jpg", "/Buggy.jpg"]
-  },
+  
 
   
 ]
