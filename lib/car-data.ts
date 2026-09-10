ff07c780e51cd79a9918f15bb5434ac5f2d50a0d
jonathanchocados123 <jonathanchocados@gmail.com>
2026-09-09 20:17:06 -0500

    Update car-data.ts

--- lib/car-data.ts
+++ lib/car-data.ts
@@ -14,38 +14,38 @@ export interface Car {
 }
 
 export const cars: Car[] = [
-    {
+  {
     id: 1,
-    marca: "Suzuki",
-    modelo: "Swift",
-    año: 2025,
-    precio: "$6.990.000",
-    kilometraje: "18 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 15 millones.",
-    nuevo: true,
-    imagenes: ["/55.jpg", "/110.jpg", "/113.jpg", "/115.jpg", "/61.jpg"]
-  },
- {
-    id: 2,
-    marca: "Renault",
-    modelo: "Koleos",
-    año: 2015,
-    precio: "$3.490.000",
-    kilometraje: "130 mil kilómetros",
+    marca: "Nissan",
+    modelo: "Navara",
+    año: 2021,
+    precio: "$9.990.000",
+    kilometraje: "30 mil kilómetros",
     estado: "Se va andando",
     airbag: "Activo airbag",
-    descripcion: "Seguro pagó 8.5 millones.",
+    descripcion: "Seguro pagó 18 millones.",
     nuevo: true,
-    imagenes: ["/111.jpg", "/112.jpg", "/36.jpg", "/38.jpg", "/88.jpg"]
+    imagenes: ["/2.jpg", "/3.jpg", "/124.jpg", "/87.jpg"]
   },
-     {
+   {
+    id: 2,
+    marca: "Renault",
+    modelo: "Fluence",
+    año: 2015,
+    precio: "$2.990.000",
+    kilometraje: "50 mil kilómetros",
+    estado: "Se va andando",
+    airbag: "No activo airbag",
+    descripcion: "Seguro pagó 7 millones.",
+    nuevo: true,
+    imagenes: ["/4.jpg", "/11.jpg", "/27.jpg", "/28.jpg", "/123.jpg"]
+  },
+  {
     id: 3,
     marca: "Chery",
     modelo: "Tiggo 2 Pro",
     año: 2023,
-    precio: "$4.590.000",
+    precio: "$2.990.000",
     kilometraje: "38 mil kilómetros",
     estado: "Se va andando",
     airbag: "No activo airbag",
@@ -53,26 +53,12 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/39.jpg", "/40.jpg", "/80.jpg", "/81.jpg"]
   },
-  {
-    id: 4,
-    marca: "Nissan",
-    modelo: "Xtrail",
-    año: 2023,
-    precio: "$14.590.000",
-    kilometraje: "19 mil kilómetros",
-    estado: "Se va andando",
-    transmision: "Automatico",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 31 millones.",
-    nuevo: true,
-    imagenes: ["/41.jpg", "/42.jpg", "/43.jpg", "/44.jpg", "/16.jpg"]
-  },
  {
-    id: 5,
+    id: 4,
     marca: "Changan",
     modelo: "Alsvin",
     año: 2021,
-    precio: "$3.690.000",
+    precio: "$3.490.000",
     kilometraje: "44 mil kilómetros",
     estado: "Se va andando",
     airbag: "No activo airbag",
@@ -80,38 +66,12 @@ export const cars: Car[] = [
     nuevo: true,
     imagenes: ["/21.jpg",  "/1.jpg", "/108.jpg", "/109.jpg"]
   },
-   {
-    id: 6,
-    marca: "Hyundai",
-    modelo: "Accent",
-    año: 2023,
-    precio: "$5.990.000",
-    kilometraje: "140 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "No activo airbag",
-    descripcion: "Seguro pagó 11 millones.",
-    nuevo: true,
-    imagenes: ["/31.jpg", "/32.jpg", "/19.jpg", "/84.jpg"]
-  },
-  {
-    id: 7,
-    marca: "Hyundai",
-    modelo: "Grand i10",
-    año: 2025,
-    precio: "$5.790.000",
-    kilometraje: "12 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "Activo airbag",
-    descripcion: "Seguro pagó 12 millones.",
-    nuevo: true,
-    imagenes: ["/37.jpg", "/116.jpg", "/123.jpg", "/87.jpg"]
-  },
 {
-    id: 8,
+    id: 5,
     marca: "Kia",
     modelo: "Soluto",
     año: 2020,
-    precio: "$4.390.000",
+    precio: "$3.990.000",
     kilometraje: "128 mil kilómetros",
     estado: "Se va andando",
     airbag: "No activo airbag",
@@ -120,11 +80,11 @@ export const cars: Car[] = [
     imagenes: ["/92.jpg", "/100.jpg", "/101.jpg", "/107.jpg", "/60.jpg"]
   },
   {
-    id: 9,
+    id: 6,
     marca: "Nissan",
     modelo: "Xtrail",
     año: 2020,
-    precio: "$6.490.000",
+    precio: "$5.990.000",
     kilometraje: "95 mil kilómetros",
     estado: "Se va andando",
     airbag: "No activo airbag",
@@ -133,37 +93,11 @@ export const cars: Car[] = [
     imagenes: ["/103.jpg", "/25.jpg", "/26.jpg", "/20.jpg", "/53.jpg"]
   },
    {
-    id: 10,
-    marca: "Mazda",
-    modelo: "Cx5 R",
-    año: 2019,
-    precio: "$5.690.000",
-    kilometraje: "89 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "Activo airbag",
-    descripcion: "Seguro pagó 16 millones.",
-    nuevo: true,
-    imagenes: ["/2.jpg", "/3.jpg", "/124.jpg"]
-  },
-  {
-    id: 11,
-    marca: "Seat",
-    modelo: "Ateca",
-    año: 2024,
-    precio: "$5.990.000",
-    kilometraje: "50 mil kilómetros",
-    estado: "Se va andando",
-    airbag: "Activo airbag",
-    descripcion: "Seguro pagó 16 millones.",
-    nuevo: true,
-    imagenes: ["/4.jpg", "/11.jpg", "/27.jpg", "/28.jpg"]
-  },
-   {
-    id: 12,
+    id: 7,
     marca: "Nissan",
     modelo: "Sentra",
     año: 2012,
-    precio: "$1.990.000",
+    precio: "$1.790.000",
     kilometraje: "99 mil kilómetros",
     estado: "Se va andando",
     airbag: "Activo airbag",
@@ -172,11 +106,11 @@ export const cars: Car[] = [
     imagenes: ["/64.jpg", "/65.jpg", "/66.jpg", "/67.jpg"]
   },
    {
-    id: 13,
+    id: 8,
     marca: "Volvo",
     modelo: "Xc60 ll T5",
     año: 2019,
-    precio: "$10.490.000",
+    precio: "$9.990.000",
     kilometraje: "87 mil kilómetros",
     estado: "Se va andando",
     airbag: "Activo airbag",
@@ -185,11 +119,11 @@ export const cars: Car[] = [
     imagenes: ["/24.jpg", "/50.jpg", "/51.jpg", "/52.jpg"]
   },
   {
-    id: 14,
+    id: 9,
     marca: "Volkswagen",
     modelo: "Nivus Tsi",
     año: 2023,
-    precio: "$7.690.000",
+    precio: "$7.590.000",
     kilometraje: "38 mil kilómetros",
     estado: "Se va andando",
     airbag: "No activo airbag",
@@ -199,11 +133,11 @@ export const cars: Car[] = [
     imagenes: ["/54.jpg", "/63.jpg", "/69.jpg", "/75.jpg", "/17.jpg"]
   },
   {
-    id: 15,
+    id: 10,
     marca: "Chery",
     modelo: "Tiggo 2",
     año: 2022,
-    precio: "$3.990.000",
+    precio: "$3.890.000",
     kilometraje: "80 mil kilómetros",
     transmision: "(Automático)",
     estado: "Funcionando",
